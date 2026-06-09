2c346c9f063424274f204802fcd47181632faa97
prestonotserp <69771424+prestonotserp@users.noreply.github.com>
2026-06-09 13:09:22 -0500

    Create input.lua

---
+++ input.lua
@@ -0,0 +1,21 @@
+hl.config({
+    input = {
+        kb_layout  = "us",
+        kb_variant = "",
+        kb_model   = "",
+        kb_options = "",
+
+        numlock_by_default = true,
+        follow_mouse = 1,
+        mouse_refocus = false,
+
+        accel_profile = "flat",
+        sensitivity = 1,
+
+        touchpad = {
+            natural_scroll = false,
+            scroll_factor = 1.0,
+            disable_while_typing = false,
+        },
+    },
+})
